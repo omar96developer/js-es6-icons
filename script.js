e3dc1ff92c7ca9e5a949b12d619821a9993a60af
omar96developer <omakino@hotmail.it>
2021-10-31 22:16:58 +0100

    FINISH

--- script.js
+++ script.js
@@ -101,63 +101,62 @@ const icone = [
 const iconCont = document.getElementById("icon_container");
 const iconSelector = document.getElementById('icon_selector');
 
+//INVOCO LA FUNZIONE PER LISTARE LE ICONE
+iconListing()
 
-
-icone.forEach ((element) => {
-	//iconCont.innerHTML += `<div class="icona"><i class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
-	if(element.type == 'animal'){
-		iconCont.innerHTML += `<div class="icona" ><i style="color:blue" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
-	} else if (element.type == 'vegetable'){
-		iconCont.innerHTML += `<div class="icona" ><i style="color:yellow" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
-
-	} else if (element.type == 'user'){
-		iconCont.innerHTML += `<div class="icona" ><i style="color:purple" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
-	} 
-}); 
- 
 iconSelector.addEventListener('change', function(){
+	//SELEZIONE OPZIONE SELEZIONATA/CAMBIATA
 	const sel = this.value;
+	//CREO I CASI POSSIBILI CON QUI AFFERAMERE IL VALUE SELEZIONATO
 	const all = 'All';
 	const animal = 'Animal';
 	const vegetables = 'Vegetables';
 	const user = 'User';
 	
 	if(sel == all){
+		//CANCELLO HTML
 		icon_container.innerHTML= "";
-		icone.forEach ((element) => {
-			if(element.type == 'animal'){
-				iconCont.innerHTML += `<div class="icona" ><i style="color:blue" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
-			} else if (element.type == 'vegetable'){
-				iconCont.innerHTML += `<div class="icona" ><i style="color:yellow" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
-		
-			} else if (element.type == 'user'){
-				iconCont.innerHTML += `<div class="icona" ><i style="color:purple" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
-			} 
-		}); 
-		
+		//STAMPO LA FUNZIONI ICONLISTING CHE MI LISTA TUTTE LE ICONE
+		iconListing()	
 	} else if(sel == animal){
 		icon_container.innerHTML= "";
+		//CICLO SUL ARRAY ICONE E LISTO LE ICONE DI TIPO ANIMAL
 		icone.forEach ((element) => {
 			if(element.type == 'animal'){
 				iconCont.innerHTML += `<div class="icona" ><i style="color:blue" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
 			} 
 		}); 
-		
 	} else if(sel == vegetables){
 		icon_container.innerHTML= "";
+		//CICLO SUL ARRAY ICONE E LISTO LE ICONE DI TIPO VEGETABLE
 		icone.forEach ((element) => {
 			if (element.type == 'vegetable'){
 				iconCont.innerHTML += `<div class="icona" ><i style="color:yellow" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
 			} 
 		}); 
-		
 	} else if(sel == user){
 		icon_container.innerHTML= "";
+		//CICLO SUL ARRAY ICONE E LISTO LE ICONE DI TIPO USER
 		icone.forEach ((element) => {
 			if (element.type == 'user'){
 				iconCont.innerHTML += `<div class="icona" ><i style="color:purple" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
 			} 
 		}); 
-		
 	};
 });
+
+//FUNZIONI
+
+function iconListing (){
+	icone.forEach ((element) => {
+		
+		if(element.type == 'animal'){
+			iconCont.innerHTML += `<div class="icona" ><i style="color:blue" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+		} else if (element.type == 'vegetable'){
+			iconCont.innerHTML += `<div class="icona" ><i style="color:yellow" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+	
+		} else if (element.type == 'user'){
+			iconCont.innerHTML += `<div class="icona" ><i style="color:purple" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+		} 
+	});
+}
