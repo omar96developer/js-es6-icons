e748da61f87f7b0347df1b8aae62b48efcb80fcd
omar96developer <omakino@hotmail.it>
2021-10-31 20:56:53 +0100

    finish

--- script.js
+++ script.js
@@ -96,11 +96,12 @@ const icone = [
 		family: 'fas'
 	}
 ];
-//Per ogni elemento del array devo prendere prefix-family-name 
-//creo un div con questi dati
-//inserisco il div nel mio html
+
 
 const iconCont = document.getElementById("icon_container");
+const iconSelector = document.getElementById('icon_selector');
+
+
 
 icone.forEach ((element) => {
 	//iconCont.innerHTML += `<div class="icona"><i class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
@@ -111,8 +112,52 @@ icone.forEach ((element) => {
 
 	} else if (element.type == 'user'){
 		iconCont.innerHTML += `<div class="icona" ><i style="color:purple" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
-
 	} 
-})
-
-
+}); 
+ 
+iconSelector.addEventListener('change', function(){
+	const sel = this.value;
+	const all = 'All';
+	const animal = 'Animal';
+	const vegetables = 'Vegetables';
+	const user = 'User';
+	
+	if(sel == all){
+		icon_container.innerHTML= "";
+		icone.forEach ((element) => {
+			if(element.type == 'animal'){
+				iconCont.innerHTML += `<div class="icona" ><i style="color:blue" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+			} else if (element.type == 'vegetable'){
+				iconCont.innerHTML += `<div class="icona" ><i style="color:yellow" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+		
+			} else if (element.type == 'user'){
+				iconCont.innerHTML += `<div class="icona" ><i style="color:purple" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+			} 
+		}); 
+		
+	} else if(sel == animal){
+		icon_container.innerHTML= "";
+		icone.forEach ((element) => {
+			if(element.type == 'animal'){
+				iconCont.innerHTML += `<div class="icona" ><i style="color:blue" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+			} 
+		}); 
+		
+	} else if(sel == vegetables){
+		icon_container.innerHTML= "";
+		icone.forEach ((element) => {
+			if (element.type == 'vegetable'){
+				iconCont.innerHTML += `<div class="icona" ><i style="color:yellow" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+			} 
+		}); 
+		
+	} else if(sel == user){
+		icon_container.innerHTML= "";
+		icone.forEach ((element) => {
+			if (element.type == 'user'){
+				iconCont.innerHTML += `<div class="icona" ><i style="color:purple" class="${element.family} ${element.prefix}${element.name}"></i><p>${element.name}</p></div>`
+			} 
+		}); 
+		
+	};
+});
